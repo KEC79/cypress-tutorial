3f8168ef139992e948ea5920ff9b2da230c9d673
Kim <mrsshoes2@gmail.com>
2020-02-09 22:18:46 +0000

    POM refactor part 1

--- cypress/integration/e2e-tests/feedback.spec.js
+++ cypress/integration/e2e-tests/feedback.spec.js
@@ -1,32 +1,26 @@
+import HomePage from "../../page-objects/HomePage"
+import Feedback from "../../page-objects/FeedbackPage"
+import FeedbackPage from "../../page-objects/FeedbackPage"
+
 describe("Feedback Test", () => {
+    const homePage = new HomePage()
+    const feedbackPage = new FeedbackPage()
+
     before(function() {
-        cy.visit("http://zero.webappsecurity.com/index.html")
-        cy.url().should("include", "index.html")
+        homePage.visit()
     })
 
     it("should display feedback form", function() {
-        cy.get("#feedback").as("Link_Feedback")
-
-        cy.get("@Link_Feedback").click()
-        cy.get("#feedback-title").should("be.visible")
-        cy.get("form").should("be.visible")
+        homePage.clickOnFeedback()
+        feedbackPage.feedbackFormIsVisible()
     })
 
     it("should submit feedback form", function() {
-        cy.get("#name").as("Input_Name")
-        cy.get("#email").as("Input_Email")
-        cy.get("#subject").as("Input_Subject")
-        cy.get("#comment").as("Input_Comment")
-        cy.get(".btn-signin").as("Submit_Button")
-
-        cy.get("@Input_Name").type("Kim")
-        cy.get("@Input_Email").type("test@test.com")
-        cy.get("@Input_Subject").type("This is a subject")
-        cy.get("@Input_Comment").type("This is a comment")
-        cy.get("@Submit_Button").click()
+        feedbackPage.fillFeedbackForm("kim", "test@test.com", "this is a subject", "this is a comment")
+        feedbackPage.submitFeedbackForm()
     })
 
     it("should display success message", function() {
-        cy.get("#feedback-title").should("be.visible")
+        feedbackPage.showSuccessMessage()
     })
 })
---
+++ cypress/integration/e2e-tests/po.spec.js
@@ -0,0 +1,10 @@
+import HomePage from "../../page-objects/HomePage"
+
+describe("HomePage test", () => {
+    it("Homepage should work", function() {
+        const homePage = new HomePage()
+        homePage.visit()
+        homePage.pause(3000)
+        homePage.carouselIsVisible()
+    })
+})
