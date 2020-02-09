609db6ba0b671f093675ac1c45a833e76ce20072
Kim <mrsshoes2@gmail.com>
2020-02-09 20:36:12 +0000

    updated cypress and added new login test

--- cypress/integration/e2e-tests/login.spec.js
+++ cypress/integration/e2e-tests/login.spec.js
@@ -1,4 +1,4 @@
-describe("Login Test", () => {
+describe("Login Failed Test", () => {
     before(function() {
         cy.visit("http://zero.webappsecurity.com/index.html")
         cy.url().should("include", "index.html")
@@ -23,6 +23,38 @@ describe("Login Test", () => {
         cy.get(".alert-error").as("Error_message")
         cy.get("@Error_message").should("be.visible")
     
+    })
+
+})
+
+describe("Login Success Test", () => {
+    before(function() {
+        cy.visit("http://zero.webappsecurity.com/index.html")
+        cy.url().should("include", "index.html")
+    })
+
+    it("should display login form", function() {
+       cy.get("#signin_button").click()
+       cy.get("#login_form").should("be.visible")
+    })
+
+    it("should login to application", function() {
+        cy.loginToApp("username", "password")
+    })
+
+    it("should display navbar links after login", function() {
+        cy.get('#account_summary_tab').should("be.visible")
+        cy.get("#account_activity_tab").should("be.visible")
+        cy.get("#transfer_funds_tab").should("be.visible")
+        cy.get("#pay_bills_tab").should("be.visible")
+        cy.get("#money_map_tab").should("be.visible")
+        cy.get("#online_statements_tab").should("be.visible")    
+    })
+
+    it("should logout from application", function() {
+        cy.get(".icon-user").click()
+        cy.get("#logout_link").click()
+        cy.get("#homeMenu").should('be.visible')
     })
 
 })
